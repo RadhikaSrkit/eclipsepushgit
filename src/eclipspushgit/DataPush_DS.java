96add58317bcba02f0af641e32ba56e48a726433
radhika <radhikaankala@gmail.com>
2023-11-01 15:26:04 +0530

    The project was commited for the First time

---
+++ src/eclipspushgit/DataPush_DS.java
@@ -0,0 +1,11 @@
+package eclipspushgit;
+
+public class DataPush_DS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("The data from eclipse IDE has to be pushed to GitHub");
+
+	}
+
+}
